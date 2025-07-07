365b65fd264c307227e938d2b39be137098c399e
AnSangjun <sangjun0319@gmail.com>
2025-07-07 13:30:55 +0900

    feat: Draw map
    drawing map function and mino

--- Hooking/NotepadHook/Tetris.cpp
+++ Hooking/NotepadHook/Tetris.cpp
@@ -1,56 +1,127 @@
 // dllmain.cpp
 #include <windows.h>
+#include <cstdlib>
+#include <ctime>
+#include <stdio.h>
 
 static WNDPROC g_OrigEditProc = nullptr;   // 원래 Edit 창 프로시저 백업
 
 constexpr int W = 10;     // 가로 10
 constexpr int H = 20;     // 세로 20
 
-static wchar_t board[H][W + 1];   // +1 은 널 종단
-static POINT   piecePos = { W/2, 0 };   // 내려오는 블록(■) 좌표
+static int board[H][W + 1];
+static POINT   piecePos = { 3, 0 };  // x = 1 ~ 10
 static HWND hEdit = nullptr;  // Edit 컨트롤 핸들
+static bool suppress_selection = false;
+static bool isMinoGenerated = false;
+static int currentMinoIndex = 0;
+
+int mino[7][4][4] = 
+{
+    {
+        {0, 0, 1, 0},
+        {0, 0, 1, 0},
+        {0, 0, 1, 0},
+        {0, 0, 1, 0}
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 1, 1, 0},
+        {0, 1, 1, 0}
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 1, 1, 0},
+        {0, 0, 1, 1}
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 1, 1},
+        {0, 1, 1, 0}
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 0, 1, 0},
+        {0, 0, 1, 0},
+        {0, 1, 1, 0}
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 0, 1, 0},
+        {0, 0, 1, 0},
+        {0, 0, 1, 1}
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 1, 1, 1},
+        {0, 0, 1, 0}
+    }
+};
 
 
 // 1) 새 Edit 창 프로시저 ------------------------------------------------
-LRESULT CALLBACK EditSubclassProc(HWND hWnd, UINT  uMsg, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK EditSubclassProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-    if (uMsg == WM_CHAR || uMsg == WM_KEYDOWN)
+    switch (uMsg)
     {
-        switch (wParam) {
-            case VK_UP: {
-                const wchar_t* text = L"TEST\r\nInjected from DLL!\r\n";
-                SendMessageW(hEdit, EM_SETSEL, (WPARAM)-1, (LPARAM)-1);      // move caret to end
-                SendMessageW(hEdit, EM_REPLACESEL, TRUE, (LPARAM)text);      // paste
-                break;
-            }
-            case VK_DOWN:
-                break;
-            case VK_LEFT:
-                break;
-            case VK_RIGHT:
-                break;
-            case VK_SPACE:
-                break;
-            default:
+        // 마우스 드래그 차단
+        case WM_LBUTTONDOWN:
+            suppress_selection = true;
+            SetCapture(hWnd);  // 마우스 캡처 (드래그 무효화 목적)
+            SetFocus(hWnd);    // 클릭 시 포커스도 강제로 주기
+            return 0;
+
+        case WM_LBUTTONUP:
+            suppress_selection = false;
+            ReleaseCapture();
+            return 0;
+
+        case WM_MOUSEMOVE:
+            if (suppress_selection)
                 return 0;
-        }
+            break;
+
+        // 마우스 포인터는 숨기지 않음
+        case WM_SETCURSOR:
+            // 아무 것도 하지 않으면 기본 커서가 유지됨
+            break;
+
+        // 키보드 입력 차단 (예시)
+        case WM_KEYDOWN:
+            switch (wParam) {
+                case VK_UP:
+                case VK_DOWN:
+                case VK_LEFT:
+                case VK_RIGHT:
+                case VK_SPACE:
+                    return 0;
+            }
+            break;
     }
 
-    // 원래 프로시저로 전달
+    // 기본 처리
     return CallWindowProcW(g_OrigEditProc, hWnd, uMsg, wParam, lParam);
 }
 
 DWORD WINAPI DrawMap(LPVOID)
 {
     if (!hEdit) return 0;
+    wchar_t display[H][W];
 
     // 초기 맵 설정
-    for (int i = 0; i < H; ++i) {
-        for (int j = 0; j < W; ++j) {
-            board[i][j] = L'.';
-        }
-        board[i][W] = L'\0';  // 널 종단
-    }
+    // for(int i = 0; i < H; i++) {
+    //     for(int j = 0; j < W; j++) {
+    //         if(board[i][j] == 0) 
+    //             display[i][j] = L'▣';  // 블록 표시
+    //         else
+    //             display[i][j] = L'.';
+    //     }
+    //     board[i][W] = L'\0';  // 문자열 종단
+    // }
 
     // 맵을 Edit 컨트롤에 출력
     SendMessageW(hEdit, EM_SETSEL, 0, -1);  // 전체 선택
@@ -58,11 +129,30 @@ DWORD WINAPI DrawMap(LPVOID)
 
     while (true) {
         // 맵을 문자열로 변환
-        wchar_t mapStr[H * (W + 1) + 1] = { 0 };
-        for (int i = 0; i < H; ++i) {
-            wcscat_s(mapStr, board[i]);
+        wchar_t mapStr[4095] = L"\0";
+        for (int i = 0; i < H; i++) {
+            wcscat_s(mapStr, L"■");
+            for(int j = 0; j < W; j++) // Mino 그리기
+            {
+                if(board[i][j] == 1) 
+                    wcscat_s(mapStr, L"▣");
+                else
+                    wcscat_s(mapStr, L"□");
+            }
+            // wcscat_s(mapStr, display[i]);
+            wcscat_s(mapStr, L"■");
             wcscat_s(mapStr, L"\r\n");
         }
+        for(int j = 0; j < W + 2; j++) 
+            wcscat_s(mapStr, L"■");
+        
+        wcscat_s(mapStr, L"\r\n");
+
+        // 디버깅용
+        wchar_t buffer[16];
+        swprintf_s(buffer, 16, L"%d", piecePos.y);
+        wcscat_s(mapStr, buffer);
+            
 
         // Edit 컨트롤에 출력
         SendMessageW(hEdit, EM_SETSEL, 0, -1);  // 전체 선택
@@ -74,12 +164,61 @@ DWORD WINAPI DrawMap(LPVOID)
     return 0;
 }
 
+DWORD WINAPI DropMino(LPVOID)
+{
+    while(!isMinoGenerated) {
+        Sleep(100);
+    }
+
+    while(true) {
+        piecePos.y++;
+        Sleep(1000);
+    }
+}
+
+DWORD WINAPI GenerateMino(LPVOID)
+{
+    if(isMinoGenerated) {
+        srand(static_cast<unsigned int>(time(nullptr)));
+        currentMinoIndex = rand() % 7;
+        isMinoGenerated = true;
+    }
+
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            if(mino[currentMinoIndex][i][j]) {
+                int x = piecePos.x + j;
+                int y = piecePos.y + i - 3;
+
+                if(x >= 1 && x <= W && y >= 0 && y < H) {
+                    board[y][x] = 1;  // 블록 표시
+                }
+            }
+        }
+    }
+    
+    return 0;
+}
+
+DWORD WINAPI GamePlayThread(LPVOID)
+{
+    while(!hEdit)
+        Sleep(10);
+
+    // 게임 맵 그리기 스레드 시작
+    CreateThread(nullptr, 0, DrawMap, nullptr, 0, nullptr);
+    CreateThread(nullptr, 0, GenerateMino, nullptr, 0, nullptr); // Test, 블럭 다운에 생성
+    CreateThread(nullptr, 0, DropMino, nullptr, 0, nullptr);
+
+    return 0;
+}
+
 // 2) Edit 컨트롤 찾아서 서브클래싱 -------------------------------------
 DWORD WINAPI HookThread(LPVOID)
 {
     // 메모장 메인 윈도우 & Edit 컨트롤 핸들 검색 (최대 5초 대기)
     HWND hNotepad = nullptr;
-    for (int i = 0; i < 100 && !hEdit; ++i)
+    for(int i = 0; i < 100 && !hEdit; ++i)
     {
         hNotepad = FindWindowW(L"Notepad", nullptr);
         if (hNotepad)
@@ -87,7 +226,7 @@ DWORD WINAPI HookThread(LPVOID)
 
         Sleep(50);
     }
-    if (!hEdit) return 0;
+    if(!hEdit) return 0;
 
     // 서브클래스 적용: 원래 프로시저 주소를 저장해 두어야 함
 #ifdef _WIN64
@@ -95,27 +234,6 @@ DWORD WINAPI HookThread(LPVOID)
 #else
     g_OrigEditProc = (WNDPROC)SetWindowLongW(hEdit, GWL_WNDPROC, (LONG)EditSubclassProc);
 #endif
-
-    return 0;
-}
-
-DWORD WINAPI InjectThread(LPVOID) {
-    // 1) Wait for the Notepad top-level window.
-    HWND hNotepad = nullptr;
-    for (int i = 0; i < 100 && !hNotepad; ++i) {          // 5 s timeout
-        hNotepad = FindWindowW(L"Notepad", nullptr);      // class "Notepad"
-        Sleep(50);
-    }
-    if (!hNotepad) return 0;
-
-    // 2) Grab the child Edit control.
-    HWND hEdit = FindWindowExW(hNotepad, nullptr, L"Edit", nullptr);
-    if (!hEdit) return 0;
-
-    // 3) Append text at the caret (or at the end if nothing selected).
-    const wchar_t* text = L"TEST\r\nInjected from DLL!\r\n";
-    SendMessageW(hEdit, EM_SETSEL, (WPARAM)-1, (LPARAM)-1);      // move caret to end
-    SendMessageW(hEdit, EM_REPLACESEL, TRUE, (LPARAM)text);      // paste
 
     return 0;
 }
@@ -128,7 +246,8 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD   ul_reason_for_call, LPVOID  /*lpR
     case DLL_PROCESS_ATTACH:
         DisableThreadLibraryCalls(hModule); // 성능 + 데드락 예방
         CreateThread(nullptr, 0, HookThread, nullptr, 0, nullptr);
-        // CreateThread(nullptr, 0, InjectThread, nullptr, 0, nullptr);
+        CreateThread(nullptr, 0, GamePlayThread, nullptr, 0, nullptr);
+    
         break;
 
     case DLL_PROCESS_DETACH:
